a8ee29e9561a7ccdbe8ad26ab8304a9926cecde5
WisniowskiPiotr <piotr.wisniowski@stgambit.com>
2018-02-19 18:34:53 +0100

    basic tests

---
+++ fuzzyDataRelations/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fuzzyDataRelations
+{
+    class Program
+    {
+        static string filePath = "data\\";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Statring to run!");
+            string fileName;
+            string block;
+            int time=0;
+            do
+            {
+                time++;
+                string word = GenerateRandomWord(time);
+                block = GenerateRandomBlockFromData(word);
+                fileName = filePath + word + ".cs";
+            } while (File.Exists(fileName));
+            File.WriteAllText(fileName, block);
+        }
+
+        private static string GenerateRandomWord(int time)
+        {
+            string result = string.Empty;
+            List<string> words = GetSomeWordsFromData();
+            Random random = new Random();
+            int segments = random.Next(1+ time, 5+ time);
+            for (int i = 0; i < segments; i++)
+            {
+                string chosenword= words[random.Next(0, words.Count - 1)];
+                if(chosenword.Length>2)
+                {
+                    chosenword = chosenword[0].ToString().ToUpper() + chosenword.Substring(1);
+                    result += chosenword;
+                }
+            }
+            return result;
+        }
+
+        private static string GenerateRandomBlockFromData(string name)
+        {
+            string[] paths = Directory.GetFiles(filePath, "*.cs");
+            Random random = new Random();
+            int randomInt = random.Next(0, paths.Length - 1);
+            string filename = Path.GetFileNameWithoutExtension(paths[randomInt]);
+            string text = File.ReadAllText(paths[randomInt]);
+            text = text.Replace(filename, name);
+            return text;
+        }
+
+        private static List<string> GetSomeWordsFromData()
+        {
+            List<string> words = new List<string>();
+
+            foreach (string path in Directory.GetFiles(filePath))
+            {
+                string text = File.ReadAllText(path);
+                words.AddRange( Sanitizer.SanitizeString(text));
+            }
+
+            return words;
+        }
+    }
+}
---
+++ fuzzyDataRelations/Sanitizer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace fuzzyDataRelations
+{
+    static class Sanitizer
+    {
+        static private string _goodChars = string.Empty;
+        static public string goodChars {
+            get {
+                if (string.IsNullOrWhiteSpace(_goodChars))
+                    _goodChars = GetGoodChars();
+                return _goodChars;
+            }
+        }
+
+        static private string _lowercaseChars = string.Empty;
+        static public string lowercaseChars
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_lowercaseChars))
+                    _lowercaseChars = GetLowercaseChars();
+                return _lowercaseChars;
+            }
+        }
+
+        static private string _uppercaseChars = string.Empty;
+        static public string uppercaseChars
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_uppercaseChars))
+                    _uppercaseChars = GetCapitalChars();
+                return _uppercaseChars;
+            }
+        }
+
+
+        static public List<string> SanitizeString(string text)
+        {
+            List<string> result = new List<string>();
+            char[] srcString = text.ToCharArray();
+            for (int i = 0; i < srcString.Length; i++)
+            {
+                if (!goodChars.Contains(srcString[i]))
+                {
+                    srcString[i] = ' ';
+                }
+            }
+            string processedtext = SplitCapitalLerretWords(new string(srcString), lowercaseChars, uppercaseChars);
+            string[] splited = processedtext.Split(new char[] { ' ' });
+            foreach (string s in splited)
+            {
+                int start = 0;
+                int end = s.Length - 1;
+                while (start < end && (s[start] == ' ' || s[start] == '-'))
+                {
+                    start++;
+                }
+                while (start < end && (s[end] == ' ' || s[end] == '-'))
+                {
+                    end--;
+                }
+                if (start < end)
+                {
+                    string word = (s.Substring(start, end - start + 1)).ToLowerInvariant();
+                    // split if more than one -
+                    if (word.IndexOf('-') != word.LastIndexOf('-'))
+                    {
+                        word = word.Replace('-', ' ');
+                        string[] splited2 = word.Split(new char[] { ' ' });
+                        foreach (string s2 in splited2)
+                        {
+                            result.Add(s2);
+                        }
+                    }
+                    else
+                    {
+                        result.Add(word);
+                    }
+                }
+            }
+            return result;
+        }
+
+        static private string SplitCapitalLerretWords(string text, string lowercaseChars, string uppercaseChars)
+        {
+            if (text.Length < 2)
+                return string.Empty;
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i < text.Length; i++)
+            {
+                result.Append(text[i - 1]);
+                if (uppercaseChars.Contains(text[i]) && lowercaseChars.Contains(text[i - 1]))
+                    result.Append(" ");
+            }
+            result.Append(text[text.Length - 1]);
+            return result.ToString();
+        }
+
+        static private string GetGoodChars()
+        {
+            string preprocessedText = "";
+            Regex rgx = new Regex("[\\p{L}-]");
+            for (int i = 0; i < 65536; i++)
+            {
+                Char c = Convert.ToChar(i);
+                Match m = rgx.Match("" + c);
+                if (m.Success)
+                    preprocessedText = preprocessedText + c;
+            }
+            return preprocessedText;
+        }
+
+        static private string GetCapitalChars()
+        {
+            string preprocessedText = "";
+            Regex rgx = new Regex("[\\p{Lu}]");
+            for (int i = 0; i < 65536; i++)
+            {
+                Char c = Convert.ToChar(i);
+                Match m = rgx.Match("" + c);
+                if (m.Success)
+                    preprocessedText = preprocessedText + c;
+            }
+            return preprocessedText;
+        }
+
+        static private string GetLowercaseChars()
+        {
+            string preprocessedText = "";
+            Regex rgx = new Regex("[\\p{Ll}]");
+            for (int i = 0; i < 65536; i++)
+            {
+                Char c = Convert.ToChar(i);
+                Match m = rgx.Match("" + c);
+                if (m.Success)
+                    preprocessedText = preprocessedText + c;
+            }
+            return preprocessedText;
+        }
+    }
+}
