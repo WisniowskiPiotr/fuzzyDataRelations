b4d21f32b68a81916d425d884e17401208a7a01d
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-03-01 16:14:07 +0100

    PublicStringEndregion gui

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -11,7 +11,7 @@ If( $runToday -gt 1) {
 		Add-Type -Path Program.cs, Sanitizer.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "PublicStringEndregion gui" 
+		git commit -a -m "fixing BetweenConsequentDoubleTechnique" 
 		git push origin master 
 	}  
 } 
