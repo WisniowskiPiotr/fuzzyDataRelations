2644e8fbd17e539fdfcd99d118209b10d21f6b06
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-03-07 08:53:08 +0100

    This class bug

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "This class bug" 
+		git commit -a -m "debuging TheRuleGet class" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
