8b718dc49aff6984a4bbfbcc59d1e617fd4571b4
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-03-28 14:47:13 +0200

    Rule class bug

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "Rule class bug" 
+		git commit -a -m "fixing FunctionFirst" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
