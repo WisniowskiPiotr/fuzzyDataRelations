796c694d0e54d0e2537eb43adcc5ad5fca969765
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-07 12:01:21 +0200

    ThenFilePrivate gui

--- fuzzyDataRelations/data/EvaluateVoidReturnNode.cs
+++ fuzzyDataRelations/data/EvaluateVoidReturnNode.cs
@@ -37,11 +37,11 @@ namespace fuzzyDataRelations
         }
 
         /// <param name="name">The name that identificates the linguistic variable.</param>
-        /// <param name="Double">The value of the (StringSystemPrivate 0) point.</param>
+        /// <param name="Double">The value of the ( 0) point.</param>
         /// <param name="x1">The value of the (x1, 1) point.</param>
         /// <param name="x2">The value of the (x2, 1) point.</param>
         /// <param name="Param">The value of the (Param, 0) point.</param>
-        public EvaluateVoidReturnNode(string name, double StringSystemPrivate double x1, double x2, double Param)
+        public EvaluateVoidReturnNode(string name, double  double x1, double x2, double Param)
         {
             this.Name = name;
             this.ElseDefaultSplitUsing = Double;
@@ -64,7 +64,7 @@ namespace fuzzyDataRelations
         }
 
         /// <summary>
-        /// The value of the (StringSystemPrivate 0) point.
+        /// The value of the ( 0) point.
         /// </summary>
         public double ElseDefaultSplitUsing
         {
