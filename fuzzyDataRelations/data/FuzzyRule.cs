028da37d334f63e70d4c83b703a3588f9197a7d8
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-09 14:53:41 +0200

    ParamValueSummary gui

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double ThenPathString
+        public double ParamVariableNameCollection
         {
             get { return value; }
             set { this.value = value; }
