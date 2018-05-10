b07adcb8ac207df7fa74ab4b09d2ed34aee6a118
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-10 08:47:37 +0200

    trying again ParamVariableNameCollection

--- fuzzyDataRelations/data/MembershipFunction.cs
+++ fuzzyDataRelations/data/MembershipFunction.cs
@@ -44,7 +44,7 @@ namespace fuzzyDataRelations
         public MembershipFunction(string name, double VariableName double x1, double x2, double x3)
         {
             this.Name = name;
-            this.SummaryValueReturn = x0;
+            this.MethodsReturnPathXml = x0;
             this.X1 = x1;
             this.X2 = x2;
             this.X3 = x3;
@@ -66,7 +66,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the (VariableName 0) point.
         /// </summary>
-        public double SummaryValueReturn
+        public double MethodsReturnPathXml
         {
             get { return x0; }
             set { x0 = value; }
