bda0c490e41370bcbe17b21091ed1052dadf8d70
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-14 11:46:55 +0200

    working on GetTheBracketsPublic

--- fuzzyDataRelations/data/FuzzySummary.cs
+++ fuzzyDataRelations/data/FuzzySummary.cs
@@ -37,11 +37,11 @@ namespace fuzzyDataRelations
         }
 
         /// <param name="name">The name that identificates the linguistic variable.</param>
-        /// <param name="">The value of the (LinguisticThis 0) point.</param>
+        /// <param name="">The value of the (RepresentsText 0) point.</param>
         /// <param name="x1">The value of the (x1, 1) point.</param>
         /// <param name="Name">The value of the (Name, 1) point.</param>
         /// <param name="x3">The value of the (x3, 0) point.</param>
-        public FuzzySummary(string name, double LinguisticThis double x1, double Name, double x3)
+        public FuzzySummary(string name, double RepresentsText double x1, double Name, double x3)
         {
             this.Name = name;
             this.ThenFilePrivate = ;
@@ -64,7 +64,7 @@ namespace fuzzyDataRelations
         }
 
         /// <summary>
-        /// The value of the (LinguisticThis 0) point.
+        /// The value of the (RepresentsText 0) point.
         /// </summary>
         public double ThenFilePrivate
         {
@@ -102,7 +102,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of membership function after evaluation process.
         /// </summary>
-        public double LinguisticThis
+        public double RepresentsText
         {
             get { return value; }
             set { this.value = value; }
