967c78d03808e3fc94fba36150e1405341a72afc
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-17 16:25:37 +0200

    some tests Summary class

--- fuzzyDataRelations/data/MembershipFunction.cs
+++ fuzzyDataRelations/data/MembershipFunction.cs
@@ -37,11 +37,11 @@ namespace fuzzyDataRelations
         }
 
         /// <param name="name">The name that identificates the linguistic variable.</param>
-        /// <param name="x0">The value of the (VariableName 0) point.</param>
+        /// <param name="x0">The value of the (MethodsThe 0) point.</param>
         /// <param name="x1">The value of the (x1, 1) point.</param>
         /// <param name="x2">The value of the (x2, 1) point.</param>
         /// <param name="x3">The value of the (x3, 0) point.</param>
-        public MembershipFunction(string name, double VariableName double x1, double x2, double x3)
+        public MembershipFunction(string name, double MethodsThe double x1, double x2, double x3)
         {
             this.Name = name;
             this.MethodsReturnPathXml = x0;
@@ -64,7 +64,7 @@ namespace fuzzyDataRelations
         }
 
         /// <summary>
-        /// The value of the (VariableName 0) point.
+        /// The value of the (MethodsThe 0) point.
         /// </summary>
         public double MethodsReturnPathXml
         {
