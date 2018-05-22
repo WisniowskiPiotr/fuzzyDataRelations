1f000a823d214cb4a9ce8b1c7a76aa69929e16b9
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-22 09:15:54 +0200

    debuging CollectionFunctionPathValue class

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "debuging CollectionFunctionPathValue class" 
+		git commit -a -m "next staff The" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
