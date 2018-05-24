1080c962ffe44118d13bedb707b381dafbd9f29e
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-05-24 16:31:44 +0200

    working on SummaryFirstEvaluation

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "working on SummaryFirstEvaluation" 
+		git commit -a -m "some tests CollectionForeachTokenName class" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
