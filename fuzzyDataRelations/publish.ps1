7a2efe88328c46ddf152b07de9db720fff8f3ebe
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-06-04 15:14:00 +0200

    trying again ThisFuzzy

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "trying again ThisFuzzy" 
+		git commit -a -m "some tests ElseForArea class" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
