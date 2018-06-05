4ad8730a52ea21b7d35a9eb92ca396b0d6f9cd8e
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-06-05 14:55:52 +0200

    trying again PropertiesThe

--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "trying again PropertiesThe" 
+		git commit -a -m "debuging CenterEndregionParamName class" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
