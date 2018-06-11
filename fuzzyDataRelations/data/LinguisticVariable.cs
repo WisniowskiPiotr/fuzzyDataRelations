69149dc7065680dc566567de24f97ff8258ea655
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-06-11 12:39:50 +0200

    Path class bug

--- fuzzyDataRelations/data/LinguisticVariable.cs
+++ fuzzyDataRelations/data/LinguisticVariable.cs
@@ -81,7 +81,7 @@ namespace fuzzyDataRelations
         /// </summary>
         /// <param name="membershipFunctionName">The membership function for which fuzzify the variable.</param>
         /// <returns>The degree of membership.</returns>
-        public double VariableStringMembershipString(string membershipFunctionName)
+        public double Numerator(string membershipFunctionName)
         {
             MembershipFunction membershipFunction = this.membershipFunctionCentorid.Find(membershipFunctionName);
 
