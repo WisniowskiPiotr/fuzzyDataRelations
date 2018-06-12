226fd71b10567948a5c6bc8769b0bec1fd2dcfc2
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-06-12 14:38:59 +0200

    working on System

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double CenterEndregionParamName
+        public double ClassCentoridNamespace
         {
             get { return value; }
             set { this.value = value; }
--- fuzzyDataRelations/publish.ps1
+++ fuzzyDataRelations/publish.ps1
@@ -2,7 +2,7 @@
 		Add-Type -Path Program.cs, Sanitizer.cs, FileFaker.cs  
 		[fuzzyDataRelations.Program]::Main()  
 		git add -A  
-		git commit -a -m "working on System" 
+		git commit -a -m "some tests ClassCentoridNamespace class" 
 		git push origin master 
        
 		$time = Get-Random -Minimum 100 -Maximum 8000 
