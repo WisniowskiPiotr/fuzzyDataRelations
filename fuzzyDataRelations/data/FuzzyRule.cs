0d62558409d45a89cd530582a17060c9e683bab7
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-06-29 12:29:07 +0200

    Summary gui

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double Value
+        public double PublicMembershipInferential
         {
             get { return value; }
             set { this.value = value; }
