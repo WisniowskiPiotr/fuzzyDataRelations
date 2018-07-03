c7e25a2dc77c7f74b17c7a9410196d86aeff3e75
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-07-03 16:12:46 +0200

    This class bug

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double PublicMembershipInferential
+        public double TheThisPublicNamespace
         {
             get { return value; }
             set { this.value = value; }
