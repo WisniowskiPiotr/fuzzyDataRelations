ebf9fc67ff5fbab878008b0589d992ef07782e01
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-07-06 08:50:19 +0200

    debuging BracketsValueCalculateValue class

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double BracketsValueCalculateValue
+        public double CollectionPublicNameGet
         {
             get { return value; }
             set { this.value = value; }
