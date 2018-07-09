4039fac128ba4c8b9f66b4efa9f005cc5ee0de40
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-07-09 08:49:17 +0200

    debuging ThatSetName class

--- fuzzyDataRelations/data/FuzzyRule.cs
+++ fuzzyDataRelations/data/FuzzyRule.cs
@@ -93,7 +93,7 @@ namespace fuzzyDataRelations
         /// <summary>
         /// The value of the rule after the evaluation process.
         /// </summary>
-        public double ThatSetName
+        public double ModelCaseSummary
         {
             get { return value; }
             set { this.value = value; }
